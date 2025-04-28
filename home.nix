3297d3f696103a0535d44d00afee123d13c65e42
Josef van Niekerk <josef.vanniekerk@DC-MMC9RW7P27.local>
2025-04-29 00:18:25 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,9 +1,13 @@
 { config, pkgs, ... }:
 
+let
+  dotfiles = ./dotfiles;
+in
 {
   home.username = "josef.vanniekerk";
   home.homeDirectory = "/Users/josef.vanniekerk";
 
+  home.stateVersion = "24.11";
 
   home.packages = [
     pkgs.bat
@@ -13,9 +17,13 @@
     pkgs.lazygit
     pkgs.tree
     pkgs.htop
+    pkgs.tmux
   ];
 
   home.file = {
+    ".config/ghostty/config".source = "${dotfiles}/.config/ghostty/config";
+    ".ideavimrc".source = "${dotfiles}/.ideavimrc";
+    ".tmux.conf".source = "${dotfiles}/.tmux.conf";
   };
 
   home.sessionVariables = {
