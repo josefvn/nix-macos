e0b660f3ba52895de202ffd44886f14518bf117c
Josef van Niekerk <josef.van.niekerk@gmail.com>
2025-05-01 00:12:06 +0200

    Merge home.nix into flake.nix for now

--- flake.nix
+++ flake.nix
@@ -8,23 +8,52 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, mac-app-util, ... }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
+      dotfiles = ./dotfiles;
 
     in {
-      homeConfigurations."josef.vanniekerk" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."my-nix" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [
+          mac-app-util.homeManagerModules.default
+          ({ config, pkgs, ... }: {
+            # TODO: Make the username and homeDirectory dynamic.
+            home.username = "josef.vanniekerk";
+            home.homeDirectory = "/Users/josef.vanniekerk";
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+            home.packages = [
+              pkgs.alacritty
+              pkgs.bat
+              pkgs.go-task
+              pkgs.home-manager
+              pkgs.htop
+              pkgs.jq
+              pkgs.lazydocker
+              pkgs.lazygit
+              pkgs.nerd-fonts.fira-code
+              pkgs.tmux
+              pkgs.tree
+            ];
+
+            home.file = {
+              ".config/alacritty/alacritty.toml".source = "${dotfiles}/.config/alacritty/alacritty.toml";
+              ".ideavimrc".source = "${dotfiles}/.ideavimrc";
+              ".tmux.conf".source = "${dotfiles}/.tmux.conf";
+            };
+
+            home.sessionVariables = {};
+            home.stateVersion = "24.11";
+          })
+        ];
       };
     };
 }
